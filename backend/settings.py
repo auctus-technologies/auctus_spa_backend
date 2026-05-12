65df6bb4d3502862959e37759a641dab696d301a
Ahamed Mansoor M <ahamedmansoorstratomove@gmail.com>
2026-05-12 10:37:45 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -165,8 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'mahbsubahani10@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'situerihvuobogyo')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'auctustech.cnn@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'avtjmcrwvixkawcp')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'mahbsubahani10@gmail.com')
 
 # Frontend URL for login link in emails
